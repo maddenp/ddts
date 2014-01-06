61b541426b6878f1914ef8ec373d423f3f8343f7
Paul Madden <maddenp@colorado.edu>
2014-01-06 08:51:45 -0700

    update tests.rb for explicit baseline gen/use (needs work)

--- ex/tests.rb
+++ ex/tests.rb
@@ -5,27 +5,37 @@ def die(msg)
   exit 1
 end
 
-def exe(suite,*expected)
+def exe(desc,suite,*expected)
   expected.map! { |e| Regexp::escape(e) }
-  print "Testing: #{suite}:"+" "*(30-suite.length)
+  print "Testing: #{desc}"+" "*(40-desc.length)
   ddts=File.join("..","ddts")
-  out=`#{ddts} #{suite}`.split("\n")
+  out=`DDTSOUT=#{$OUT} #{ddts} #{suite}`.split("\n")
   expected.each do |string|
     die "FAILED" if out.grep(/.*#{string}.*/).empty? 
   end
   puts "ok"
 end
 
+# Set some variables.
+
+$OUT="./tests_out"
+baseline=File.join($OUT,"baseline")
+sentinel=File.join($OUT,"builds","ex_build","sentinel")
+
+# Create a directory for test detritus.
+
+FileUtils.mkdir_p($OUT)
+
 # ex_suite_single executes the single ex_4 run, which is expected to pass.
 
-exe("ex_suite_single",
+exe("ex_suite_single","ex_suite_single",
   "Run ex_4: Completed",
   "ALL TESTS PASSED"
   )
 
 # ex_suite_fail executes the single ex_fail run, which is expected to fail.
 
-exe("ex_suite_fail",
+exe("ex_suite_fail","ex_suite_fail",
   "Run ex_fail: Run failed",
   "Test suite 'ex_suite_fail' FAILED"
   )
@@ -33,7 +43,7 @@ exe("ex_suite_fail",
 # ex_suite_build_only uses the suite-level 'build_only' setting to perform only
 # the required build, without performing any runs.
 
-exe("ex_suite_build_only",
+exe("ex_suite_build_only","ex_suite_build_only",
   "Build ex_build completed",
   "ALL TESTS PASSED"
   )
@@ -43,32 +53,31 @@ exe("ex_suite_build_only",
 # the suite, then check that the sentinel is still there to prove that the old
 # build was not deleted.
 
-f=File.join("builds","ex_build","sentinel")
-FileUtils.touch(f)
-die "Sentinel file '#{f}' was not created" unless File.exist?(f)
-exe("ex_suite_retain_builds",
+FileUtils.touch(sentinel)
+die "Sentinel file '#{sentinel}' was not created" unless File.exist?(sentinel)
+exe("ex_suite_retain_builds","ex_suite_retain_builds",
   "Comparison: ex_1, ex_1_alt, ex_2, ex_4: OK",
   "ALL TESTS PASSED"
   )
-die "Sentinel file '#{f}' missing!" unless File.exist?(f)
+die "Sentinel file '#{sentinel}' missing!" unless File.exist?(sentinel)
 
 # ex_suite executes four runs -- also create a baseline here.
 
-exe("baseline ex_suite",
+exe("ex_suite (gen baseline pass)","gen-baseline #{baseline} ex_suite",
   "Creating ex_baseline baseline: OK",
   "ALL TESTS PASSED"
   )
 
 # Retry baseline creation -- it should fail.
 
-exe("baseline ex_suite",
+exe("ex_suite (gen baseline fail)","gen-baseline #{baseline} ex_suite",
   "ex_baseline already exists",
   "Test suite 'ex_suite' FAILED"
   )
 
 # Execute ex_suite again and verify against baseline.
 
-exe("ex_suite",
+exe("ex_suite use-baseline","use-baseline #{baseline} ex_suite",
   "Run ex_1: Baseline comparison OK",
   "Comparison: ex_1, ex_1_alt, ex_2, ex_4: OK",
   "ALL TESTS PASSED"
@@ -77,8 +86,7 @@ exe("ex_suite",
 # ex_suite_1p_1f executes two runs in one group, where one fails. The successful
 # run has nothing to compare to, so comparison is skipped.
 
-exe(
-  "ex_suite_1p_1f",
+exe("ex_suite_1p_1f","ex_suite_1p_1f",
   "Group stats: 1 of 2 runs failed, skipping comparison",
   "Suite stats: Failure in 1 of 1 group(s)",
   "Failure in 1 of 1 group(s)"
@@ -87,7 +95,7 @@ exe(
 # ex_suite_2p_1f executes three runs in one group, where one fails. The two
 # successful runs are compared.
 
-exe("ex_suite_2p_1f",
+exe("ex_suite_2p_1f","ex_suite_2p_1f",
   "Comparison: ex_1, ex_2: OK",
   "Suite stats: Failure in 1 of 1 group(s)",
   "1 of 3 TEST(S) FAILED"
@@ -97,7 +105,7 @@ exe("ex_suite_2p_1f",
 # Comparison is skipped in the group with the failed run, and an alternate
 # comparator is used for the other group.
 
-exe("ex_suite_3p_1f",
+exe("ex_suite_3p_1f","ex_suite_3p_1f",
   "Group stats: 1 of 2 runs failed, skipping comparison",
   "alternate comparator",
   "Comparison: ex_1, ex_2: OK",
@@ -107,28 +115,31 @@ exe("ex_suite_3p_1f",
 
 # ex_suite_mismatch_stop contains a run that produces output different from the
 # other runs', so that comparison fails. Here, 'continue' is false and a
-# baseline is present, so the suite fails on baseline comparison of the oddball
+# baseline is used, so the suite fails on baseline comparison of the oddball
 # run.
 
 die "Cannot find 'baseline'" unless File.exist?("baseline")
-exe("ex_suite_mismatch_stop",
+exe("ex_suite_mismatch_stop (with baseline)","use-baseline #{baseline} ex_suite_mismatch_stop",
   "Run ex_4_bad: Comparison failed (ex_4_bad vs baseline ex_baseline)",
   "Test suite 'ex_suite_mismatch_stop' FAILED"
   )
 
-# Same as previous test, but delete the baseline first so that the suite fails
-# on run-vs-run comparison.
+# Same as previous test, but without baseline comparison, so that the suite
+# fails on run-vs-run comparison.
 
-FileUtils.rm_rf("baseline")
-exe("ex_suite_mismatch_stop",
+exe("ex_suite_mismatch_stop (no baseline)","ex_suite_mismatch_stop",
   "Comparison: Comparison failed (ex_2 vs ex_4_bad)",
   "Test suite 'ex_suite_mismatch_stop' FAILED"
   )
 
 # Same as previous test, but 'continue' is true so that the suite completes.
 
-exe("ex_suite_mismatch_continue",
+exe("ex_suite_mismatch_continue","ex_suite_mismatch_continue",
   "Comparison failed (ex_2 vs ex_4_bad)",
   "Suite stats: Failure in 1 of 2 group(s)",
   "0 of 4 TEST(S) FAILED"
   )
+
+# Remove output directory.
+
+FileUtils.rm_rf($OUT)
